26279b0f66880d99791cbfd9c29844c00c64dd55
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2018-01-11 14:57:23 +0100

    Show state transitions

--- tests/example_reader_read_first_page.py
+++ tests/example_reader_read_first_page.py
@@ -10,9 +10,18 @@ from tinkerforge.bricklet_nfc import BrickletNFC
 import time
 
 tag_type = 0
+old_state = None
 
 # Callback function for state changed callback
 def cb_state_changed(state, idle, nfc):
+
+    if old_state is not None:
+        print 'State transition', old_state, '-->', state
+    else:
+        print 'Initial State', state
+
+    global old_state
+    old_state = state
 
     if state == nfc.READER_STATE_IDLE:
         nfc.reader_request_tag_id()
@@ -24,6 +33,7 @@ def cb_state_changed(state, idle, nfc):
               ' with ID [' +
               ' '.join(map(str, map(hex, ret.tag_id))) +
               "]")
+        print '\n\n'
 
         if ret.tag_type == nfc.TAG_TYPE_MIFARE_CLASSIC:
             nfc.reader_authenticate_mifare_classic_page(1, 0, [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF])
@@ -36,6 +46,10 @@ def cb_state_changed(state, idle, nfc):
     elif state == nfc.READER_STATE_REQUEST_PAGE_READY:
         data = nfc.reader_read_page()
         print(len(data), data)
+        print '\n\n'
+        nfc.reader_request_tag_id()
+    else:
+        print '\n\n'
 
 if __name__ == "__main__":
     ipcon = IPConnection() # Create IP connection

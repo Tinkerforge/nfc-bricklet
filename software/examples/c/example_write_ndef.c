8e20da27629d25875c1aa4964edd48f92410f19f
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2018-01-18 11:00:24 +0100

    Add C examples

---
+++ software/examples/c/example_write_ndef.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+
+#include "ip_connection.h"
+#include "bricklet_nfc.h"
+
+#define HOST "localhost"
+#define PORT 4223
+#define UID "XYZ" // Change XYZ to the UID of your NFC Bricklet
+#define NDEF_URI "www.tinkerforge.com"
+
+// Callback function for state changed callback
+void cb_state_changed(uint8_t state, bool idle, void *user_data) {
+	NFC *nfc = (NFC *)user_data;
+
+	if(state == NFC_CARDEMU_STATE_IDLE) {
+		uint8_t i = 0;
+		uint8_t ndef_record_uri[512];
+		char *ptr_ndef_uri = NDEF_URI;
+
+		memset(ndef_record_uri, 0, 512);
+
+		// Only short records are supported
+		ndef_record_uri[0] = 0xD1;
+		ndef_record_uri[1] = 0x01;
+		ndef_record_uri[2] = strlen(NDEF_URI) + 1;
+		ndef_record_uri[3] = 'U';
+		ndef_record_uri[4] = 0x04;
+
+		while (*ptr_ndef_uri) {
+			ndef_record_uri[5 + i] = *ptr_ndef_uri;
+			i++;
+			ptr_ndef_uri++;
+		}
+
+		nfc_cardemu_write_ndef(nfc, ndef_record_uri, strlen(NDEF_URI) + 5);
+		nfc_cardemu_start_discovery(nfc);
+	}
+	else if(state == NFC_CARDEMU_STATE_DISCOVER_READY) {
+		nfc_cardemu_start_transfer(nfc, 1);
+	}
+	else if(state == NFC_CARDEMU_STATE_DISCOVER_ERROR) {
+		printf("Discover error\n");
+	}
+	else if(state == NFC_CARDEMU_STATE_TRANSFER_NDEF_ERROR) {
+		printf("Transfer NDEF error\n");
+	}
+}
+
+int main(void) {
+	// Create IP connection
+	IPConnection ipcon;
+	ipcon_create(&ipcon);
+
+	// Create device object
+	NFC nfc;
+	nfc_create(&nfc, UID, &ipcon);
+
+	// Connect to brickd
+	if(ipcon_connect(&ipcon, HOST, PORT) < 0) {
+		fprintf(stderr, "Could not connect\n");
+
+		return 1;
+	}
+	// Don't use device before ipcon is connected
+
+	// Register state changed callback to function cb_state_changed
+	nfc_register_callback(&nfc,
+	                      NFC_CALLBACK_CARDEMU_STATE_CHANGED,
+	                      (void *)cb_state_changed,
+	                      &nfc);
+
+	nfc_set_mode(&nfc, NFC_MODE_CARDEMU);
+
+	printf("Press key to exit\n");
+	getchar();
+	nfc_destroy(&nfc);
+	ipcon_destroy(&ipcon); // Calls ipcon_disconnect internally
+
+	return 0;
+}
